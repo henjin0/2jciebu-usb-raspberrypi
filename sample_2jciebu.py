0ac66fda7cfc6775262eaa95a07d905369955116
henjin0 <henjin0@gmail.com>
2019-04-18 20:47:11 +0900

    Update sample_2jciebu.py

--- sample_2jciebu.py
+++ sample_2jciebu.py
@@ -1,4 +1,3 @@
-import ambient
 import serial
 import time
 from datetime import datetime
@@ -97,26 +96,33 @@ def now_utc_str():
 
 
 if __name__ == '__main__':
-    try:
-        CHANNEL_ID = int(os.environ['AMBIENT_CHANNEL_ID'])
-        WRITE_KEY = os.environ['AMBIENT_WRITE_KEY']
-    except KeyError as e:
-        print('Missing environment variable: '.format(e))
-        exit(1)
-    am = ambient.Ambient(CHANNEL_ID, WRITE_KEY)
-    
+
     # Serial.
     ser = serial.Serial("/dev/ttyUSB0", 115200, serial.EIGHTBITS, serial.PARITY_NONE)
-    
-    last_uploaded = datetime.now()
-    while True:
-        try:
-            timestamp = datetime.now()
-            if (timestamp - last_uploaded).seconds > 10:
-                 am.send({
-                    "d1": e.get_co2(),
-                    "created": timestamp.strftime("%Y/%m/%d %H:%M:%S")
-                })
+
+    try:
+        # LED On. Color of Green.
+        command = bytearray([0x52, 0x42, 0x0a, 0x00, 0x02, 0x11, 0x51, DISPLAY_RULE_NORMALLY_ON, 0x00, 0, 255, 0])
+        command = command + calc_crc(command, len(command))
+        ser.write(command)
+        time.sleep(0.1)
+        ret = ser.read(ser.inWaiting())
+
+        while ser.isOpen():
+            # Get Latest data Long.
+            command = bytearray([0x52, 0x42, 0x05, 0x00, 0x01, 0x21, 0x50])
+            command = command + calc_crc(command, len(command))
+            tmp = ser.write(command)
+            time.sleep(0.1)
+            data = ser.read(ser.inWaiting())
+            print_latest_data(data)
             time.sleep(1)
-        except KeyboardInterrupt:
-            break
+
+    except KeyboardInterrupt:
+        # LED Off.
+        command = bytearray([0x52, 0x42, 0x0a, 0x00, 0x02, 0x11, 0x51, DISPLAY_RULE_NORMALLY_OFF, 0x00, 0, 0, 0])
+        command = command + calc_crc(command, len(command))
+        ser.write(command)
+        time.sleep(1)
+        # script finish.
+        sys.exit
