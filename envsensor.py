49c069660a772ad5a5f14a7042636ac4ba8071af
henjin0 <henjin0@gmail.com>
2019-04-18 19:52:38 +0900

    Update envsensor.py

--- envsensor.py
+++ envsensor.py
@@ -1,3 +1,6 @@
+import ambient
+import os
+from datetime import datetime
 import serial
 import threading
 import time
@@ -97,15 +100,29 @@ class EnvSensor(threading.Thread):
         self.stop = True
 
 if __name__ == '__main__':
+    try:
+        CHANNEL_ID = int(os.environ['AMBIENT_CHANNEL_ID'])
+        WRITE_KEY = os.environ['AMBIENT_WRITE_KEY']
+    except KeyError as e:
+        print('Missing environment variable: '.format(e))
+        exit(1)
+    am = ambient.Ambient(CHANNEL_ID, WRITE_KEY)
+    
+    last_uploaded = datetime.now()
+    
     # EnvSensorクラスの実体を作成します
     e = EnvSensor()
     # スレッドとして処理を開始します
     e.start()
-
+    
     while True:
         try:
-            time.sleep(10)
-            # CO2データを取得し、print関数で表示します
-            print("eCO2: {}".format(e.get_co2()))
+            timestamp = datetime.now()
+            if (timestamp - last_uploaded).seconds > 10:
+                 am.send({
+                    "d1": e.get_co2(),
+                    "created": timestamp.strftime("%Y/%m/%d %H:%M:%S")
+                })
+            time.sleep(1)
         except KeyboardInterrupt:
             break
